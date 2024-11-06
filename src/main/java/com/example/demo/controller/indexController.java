4baac6ab43bdb410f2c9356c422515878c826b3c
Searriagada <sebastian.arriag93@gmail.com>
2024-11-06 17:48:37 -0300

    se crean fragmentos navbar y head, pages index y listar

--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -1,5 +1,18 @@
 package com.example.demo.controller;
 
-public class indexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class indexController {
+	@GetMapping({"/","index"})
+	public String inicio(Model model) {
+		return "index";
+	}
+	
+	@GetMapping({"nosotros"})
+	public String nosotros(Model model) {
+		return "/forms/nosotros";
+	}
 }
