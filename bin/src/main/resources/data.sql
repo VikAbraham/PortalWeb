5cdb4b88a07e168cd4c84916b67ad7c2f329a70d
VictorSilva <victor.silva.pradenas@ciisa.cl>
2024-11-07 13:03:14 -0300

    Creacion de formularios con rutas y url

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO pelicula (titulo, anio, director, genero, sinopsis, duracion, imagen, create_at)
+VALUES
+('El Padrino', 1972, 'Francis Ford Coppola', 'Drama', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',   
+ 175, '', '1972-03-14'),
+ ('El Padrino', 1972, 'Francis Ford Coppola', 'Drama', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',   
+ 175, '', '1972-03-14');
--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -11,8 +11,14 @@ public class indexController {
 		return "index";
 	}
 	
-	@GetMapping({"nosotros"})
-	public String nosotros(Model model) {
-		return "/forms/nosotros";
+	@GetMapping({"formularioUsuario"})
+	public String usuario(Model model) {
+		return "/forms/usuario";
 	}
+	
+	@GetMapping({"formularioResena"})
+	public String resena(Model model) {
+		return "/forms/resena";
+	}
+	
 }
