1a01cdbdda105f04672ccbb98015a9b4acc32f79
Searriagada <sebastian.arriag93@gmail.com>
2024-11-17 11:14:27 -0300

    se completa crud con agregar, editar, eliminar

--- src/main/java/com/example/demo/controller/ResenaController.java
+++ src/main/java/com/example/demo/controller/ResenaController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Resena;
 import com.example.demo.service.ResenaService;
@@ -20,6 +22,7 @@ import com.example.demo.serviceImpl.ResenaServiceImpl;
  */
 
 @Controller
+@RequestMapping("/resena")
 public class ResenaController {
 
 	@Autowired
@@ -33,7 +36,7 @@ public class ResenaController {
 	 * @return a la vista donde se mostrarán las resenas
 	 * 
 	 **/
-	@GetMapping({ "/resena" })
+	@GetMapping({ "/listar" })
 	public String listarResena(Model model) {
 		List<Resena> resenas = resenaService.findAll();
 
@@ -54,16 +57,34 @@ public class ResenaController {
 		return "forms/nueva-resena";
 	}
 
-	@PostMapping("/guardarResena")
+	@PostMapping("/agregar")
 	public String guardarResena(@ModelAttribute Resena resena) {
 		resenaService.save(resena);
-		return "redirect:/resena";
+		return "redirect:/resena/listar";
 	}
 	
-	@GetMapping({ "/editarResena" })
-	public String editarResena(Model model) {
+	@GetMapping({ "/editar/{id}" })
+	public String editarResena(@PathVariable("id") Long id,Model model) {
 		model.addAttribute("titulo", "Editar Reseña");
-		return "forms/nueva-resena";
+		Resena resena = resenaService.findOne(id);
+		if(resena !=  null) {
+			model.addAttribute("resena", resena);
+			return "forms/nueva-resena";
+		}else {
+			return "redirect:/resena";
+		}
 	}
+	
+	@GetMapping({ "/eliminar/{id}" })
+	public String eliminarResena(@PathVariable("id") Long id,Model model) {
+		Resena resena = resenaService.findOne(id);
+		if(resena !=  null) {
+			resenaService.delete(id);
+			return "redirect:/resena/listar";
+		}else {
+			return "redirect:/resena/listar";
+		}
+	}
+	
 
 }
